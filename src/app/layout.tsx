4b8232257413092093db8604e3db1f8319d0694c
navasshereef1 <125650169+navasshereef1@users.noreply.github.com>
2025-12-31 13:35:00 +0400

    Integrate Meta Pixel tracking in layout
    Added Meta Pixel tracking script and fallback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script"; // Imported Script component
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Meta Pixel Script */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1911584746404336');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      
       <body
         suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
+        {/* Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1911584746404336&ev=PageView&noscript=1"
+            alt="meta pixel"
+          />
+        </noscript>
+
         <AnalyticsTracker />
         <Navbar />
         {children}
